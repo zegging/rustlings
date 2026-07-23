75a7064d44fee0ae451f2784f1e235c138ebdbb4
huiti <huiti@qunhemail.com>
2026-07-23 20:41:53 +0800

    fix: complete ice cream function logic and update tests for options exercises

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,6 +4,13 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    if hour_of_day > 23 {
+        None
+    } else if hour_of_day >= 22 {
+        Some(0)
+    } else {
+        Some(5)
+    }
 }
 
 fn main() {
@@ -20,7 +27,7 @@ mod tests {
         // Option?
         let ice_creams = maybe_ice_cream(12);
 
-        assert_eq!(ice_creams, 5); // Don't change this line.
+        assert_eq!(ice_creams, Some(5)); // Don't change this line.
     }
 
     #[test]
