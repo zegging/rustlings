b2b0c29e1043a965ede68c72f99defab184cd4d0
huiti <huiti@qunhemail.com>
2026-07-23 15:40:00 +0800

    fix: make functions public in modules to resolve access issues

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -6,6 +6,9 @@ mod delicious_snacks {
     // use self::fruits::PEAR as ???;
     // use self::veggies::CUCUMBER as ???;
 
+    pub use self::fruits::PEAR as fruit;
+    pub use self::veggies::CUCUMBER as veggie;
+
     mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
