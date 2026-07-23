f5631659ad412355646360089c18cb339c3b8414
huiti <huiti@qunhemail.com>
2026-07-23 18:32:47 +0800

    fix: update fruit basket and scoring logic in hash map exercises

--- exercises/11_hashmaps/hashmaps3.rs
+++ exercises/11_hashmaps/hashmaps3.rs
@@ -31,7 +31,16 @@ fn build_score_table(results: &str) -> HashMap<&str, TeamScores> {
         // Keep in mind that goals scored by team 1 will be the number of goals
         // conceded by team 2. Similarly, goals scored by team 2 will be the
         // number of goals conceded by team 1.
-    }
+        // Insert the default with zeros if a team doesn't exist yet.
+        let team_1 = scores.entry(team_1_name).or_default();
+        // Update the values.
+        team_1.goals_scored += team_1_score;
+        team_1.goals_conceded += team_2_score;
+
+        // Similarly for the second team.
+        let team_2 = scores.entry(team_2_name).or_default();
+        team_2.goals_scored += team_2_score;
+        team_2.goals_conceded += team_1_score;    }
 
     scores
 }
