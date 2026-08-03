7509a8face4d65a47958958c93f098a9d4e12450
huiti <huiti@qunhemail.com>
2026-08-03 16:16:14 +0800

    fix: implement tests for is_even and power_of_2 functions, update Rectangle tests for width and height

--- exercises/17_tests/tests2.rs
+++ exercises/17_tests/tests2.rs
@@ -15,9 +15,9 @@ mod tests {
     #[test]
     fn you_can_assert_eq() {
         // TODO: Test the function `power_of_2` with some values.
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
-        assert_eq!();
+        assert_eq!(power_of_2(0), 1u64);
+        assert_eq!(power_of_2(1), 2u64);
+        assert_eq!(power_of_2(2), 4u64);
+        assert_eq!(power_of_2(3), 8u64);
     }
 }
